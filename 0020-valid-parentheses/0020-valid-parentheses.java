7023c36dae61903c8ec96eb5fcbe80d8211b5541
Ashutosh kumar <ashutoshs87113@gmail.com>
2026-08-17 15:48:35 +0530

    Time: 4 ms (36.7%), Space: 43.5 MB (15.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -6,6 +6,10 @@ class Solution {
         if(a=='(' && b==')') return true;
         return false;
     }
+    public boolean isOpen(char a){
+        if(a == '[' || a == '{' || a == '(') return true;
+        return false;
+    }
     public boolean isValid(String s) {
         
         Stack<Character> st = new Stack<>();
@@ -13,7 +17,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
 
-            if(ch == '[' || ch == '{' || ch == '(') st.push(ch);
+            if(isOpen(ch)) st.push(ch);
 
             else{
                 if(st.size() == 0) return false;
